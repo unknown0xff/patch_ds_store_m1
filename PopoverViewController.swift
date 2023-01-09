e90263f69aecdbafdd8c9174e9f9408716d555c1
unknown0xff <66771518+unknown0xff@users.noreply.github.com>
2023-01-09 12:22:38 +0800

    Update PopoverViewController.swift

--- PopoverViewController.swift
+++ PopoverViewController.swift
@@ -20,11 +20,8 @@ class PopoverViewController: NSViewController {
 
 extension PopoverViewController {
     static func freshController() -> PopoverViewController {
-        //获取对Main.storyboard的引用
         let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
-        // 为PopoverViewController创建一个标识符
         let identifier = NSStoryboard.SceneIdentifier("PopoverViewController")
-        // 实例化PopoverViewController并返回
         guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? PopoverViewController else {
             fatalError("Something Wrong with Main.storyboard")
         }
